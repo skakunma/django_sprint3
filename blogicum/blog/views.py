8255fbe0632cf589f2e9dd22e39b6d655fa2aa31
Дмитрий Скакун <skakun2025@mail.ru>
2024-04-29 11:51:19 +0300

    second commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from blog.models import Post, Category
 
+
 def index(request):
     post_list = Post.objects.filter(
         is_published=True,
@@ -12,6 +13,7 @@ def index(request):
     template = 'blog/index.html'
     context = {'post_list': post_list}
     return render(request, template, context)
+
 
 def post_detail(request, id):
     post = get_object_or_404(
@@ -25,8 +27,10 @@ def post_detail(request, id):
     context = {'post': post}
     return render(request, template, context)
 
+
 def category_posts(request, category_slug):
-    category = get_object_or_404(Category, slug=category_slug, is_published=True)
+    category = get_object_or_404(Category,
+                                 slug=category_slug, is_published=True)
     post_list = category.post_set.filter(
         is_published=True,
         pub_date__lte=timezone.now()
