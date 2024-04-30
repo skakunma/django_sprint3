bc0e9bacef2274fb136e09f3dd6cbac867f006c0
Дмитрий Скакун <skakun2025@mail.ru>
2024-04-30 23:25:48 +0300

    last commit

--- blogicum/blog/admin.py
+++ blogicum/blog/admin.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
 from .models import Category, Location, Post
 
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ['title', 'description', 'is_published', 'created_at']
     search_fields = ['title', 'description']
     list_filter = ['is_published', 'created_at']
 
+
 class LocationAdmin(admin.ModelAdmin):
     list_display = ['name', 'is_published', 'created_at']
     search_fields = ['name']
     list_filter = ['is_published', 'created_at']
+
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'category',
